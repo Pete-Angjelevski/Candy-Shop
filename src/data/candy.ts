68832e7690d533b947dc7f4fbb7beccada5cbf14
Pete Angjelevski <p.angjelevski@gmail.com>
2021-05-08 14:34:31 +1200

    add candy data

---
+++ src/data/candy.ts
@@ -0,0 +1,103 @@
+export default {
+  beers: [
+    {
+      id: 1,
+      name: 'Candy Cane',
+      price: 2.00,
+      vegan: true,
+      gf: true,
+      description: 'These merry, minty, minis are great for snacking, baking, decorating and adorning holiday trees, gingerbread houses and special gifts.'
+    }, {
+      id: 2,
+      name: 'CupCake',
+      price: 3.50,
+      vegan: false,
+      gf: false,
+      description: ''
+    }, {
+      id: 3,
+      name: 'Lollipop',
+      price: 1.00,
+      vegan: true,
+      gf: true,
+      description: 'Our lollipops are perfectly handcrafted for your special event! With the most vibrant colors and amazing taste you will love them and so as your guests.'
+    }, {
+      id: 4,
+      name: 'Gingerbread Man',
+      price: 5.00,
+      vegan: true,
+      gf: true,
+      description: ''
+    }, {
+      id: 5,
+      name: 'Donut',
+      price: 3.00,
+      vegan: false,
+      gf: false,
+      description: ''
+    }, {
+      id: 6,
+      name: 'Macaroon',
+      price: 3.00,
+      vegan: false,
+      gf: false,
+      description: ''
+    }, {
+      id: 7,
+      name: 'Muffin',
+      price: 5.00,
+      vegan: false,
+      gf: false,
+      description: ''
+    }, {
+      id: 8,
+      name: 'Chocolate Bar',
+      price: 5.00,
+      vegan: false,
+      gf: false,
+      description: 'Scrumptious chocolate bars with almonds that are perfect to eat straight out of the wrapper or to make delectable smores treats'
+    }, {
+      id: 9,
+      name: 'Apple Pie',
+      price: 25.00,
+      vegan: false,
+      gf: false,
+      description: ''
+    }, {
+      id: 10,
+      name: 'Jell-O',
+      price: 1.00,
+      vegan: false,
+      gf: false,
+      description: 'The perfect choice for satisfying a sweet tooth while maintaining a healthy lifestyle.'
+    }, {
+      id: 11,
+      name: 'Pancakes',
+      price: 14.00,
+      vegan: false,
+      gf: false,
+      description: 'We conducted double-blind taste tests on every organic flour in America, and handpicked the remaining ingredients to ensure only the highest quality. The result? The fluffiest, tastiest pancakes imaginable.'
+    }, {
+      id: 12,
+      name: 'Pretzels',
+      price: 8.00,
+      vegan: false,
+      gf: false,
+      description: 'Pretzel twists are dusted with our top-secret seasoning blend, giving our snacks a unique, unforgettable flavor. Every bite envelops your taste buds in a delightful swirl of buttery, sweet, and spicy tang. They’re the snack you never knew you needed.'
+    }, {
+      id: 13,
+      name: 'Wrapped Candy',
+      price: 5.00,
+      vegan: true,
+      gf: true,
+      description: 'Sweeten the moment with sugar-free hard candy. Made with Splenda, these individually wrapped treats are perfect for holiday parties or sharing with family & friends'
+    }, {
+      id: 14,
+      name: 'Cinnamon Twist',
+      price: 4.50,
+      vegan: false,
+      gf: false,
+      description: ''
+    }
+  ]
+}
